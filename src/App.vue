0b3e84c038b55430e299f1eff27bfcb751373106
YasinDehfuli <ydehfuli@gmail.com>
2024-04-17 14:33:47 +0330

    change location and rtl providing from internal providing to globally providing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
+<!--  <v-locale-provider rtl locale="fa">-->
   <v-app>
     <v-main>
-      <HelloWorld />
+       <body-component ></body-component>
+      <v-data-table :items="[]"></v-data-table>
     </v-main>
   </v-app>
+<!--  </v-locale-provider>-->
 </template>
 
 <script setup lang="ts">
-  //
+
 </script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/main.scss";
+</style>
