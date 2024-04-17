bfade9a6711b55e30ef8c5c91102dbfa225bd089
YasinDehfuli <ydehfuli@gmail.com>
2024-04-17 12:17:35 +0330

    create new components & use it in app

---
+++ src/components/BodyComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  
+
+  <carousel/>
+
+  <card-white>
+  </card-white>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+let id = 0;
+const images = ref<
+  {id:number, src:string}[]>([
+  { id: id++, src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg" },
+  { id: id++, src: 'https://cdn.vuetifyjs.com/images/cards/hotel.jpg' },
+  { id: id++, src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg' }
+])
+</script>
+
+<template>
+  <v-carousel>
+    <v-carousel-item v-for="image in images" key="image.id" :src="image.src">
+
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<style scoped>
+
+</style>
